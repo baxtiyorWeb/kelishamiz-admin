22e51b6b329b6e9141608137799cc37e8184795c
baxtiyoeWeb <baxtiyorqurbonnazarov33@gmail.com>
2024-03-05 07:51:28 +0500

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={inter.className}>
 				<Providers>
-					<NextProgress />
+					<NextProgress height={5} showSpinner={false} />
 					<Header />
 					<Navbar />
 					<main className='main'>{children}</main>
