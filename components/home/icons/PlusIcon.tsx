3d4980821c713386e7cd8332a6956a3ae50b6def
baxtiyoeWeb <baxtiyorqurbonnazarov33@gmail.com>
2024-03-05 06:17:20 +0500

    ok updated

--- components/home/icons/PlusIcon.tsx
+++ components/home/icons/PlusIcon.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
-export const PlusIcon = ({ size = 24, width, height, ...props }) => (
+interface IProps {
+	size: number;
+	width: any;
+	height: any;
+	props: React.ReactNode;
+}
+
+export const PlusIcon: React.FC<IProps> = ({
+	size = 24,
+	width,
+	height,
+	...props
+}) => (
 	<svg
 		aria-hidden='true'
 		fill='none'
