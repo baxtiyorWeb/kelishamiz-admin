40f541f29b2c103042e9c4145d3c84b255632572
baxtiyoeWeb <baxtiyorqurbonnazarov33@gmail.com>
2024-03-05 06:10:07 +0500

    ok

--- components/home/homeDataTable.tsx
+++ components/home/homeDataTable.tsx
@@ -167,7 +167,11 @@ export default function HomeTableData() {
 						<Dropdown className='bg-background border-1 border-default-200'>
 							<DropdownTrigger>
 								<Button isIconOnly radius='full' size='sm' variant='light'>
-									<VerticalDotsIcon className='text-default-400' />
+									<VerticalDotsIcon
+										width={50}
+										height={50}
+										className='text-default-400'
+									/>
 								</Button>
 							</DropdownTrigger>
 							<DropdownMenu>
