8d5113ca7cb59f9be14504eb77736e245b0de718
baxtiyoeWeb <baxtiyorqurbonnazarov33@gmail.com>
2024-03-08 19:47:27 +0500

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,28 @@
 'use client';
 import Home from '@@/components/home/home';
-import UserProvider, { UserContext } from '@@/context/UserContext';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import { useContext, useEffect } from 'react';
+import UserProvider from '@@/context/UserContext';
 export default function page() {
-	const { user } = useContext(UserContext);
-	const navigate = useRouter();
-	useEffect(() => {
-		try {
-			const data = axios
-				.get('http://kelishamiz.uz/api/v1/user/1', {
-					headers: {
-						Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-					},
-				})
-				.then(res => console.log(res.data))
-				.catch(error => {
-					console.log(error);
+	// const { user } = useContext(UserContext);
+	// const navigate = useRouter();
+	// useEffect(() => {
+	// 	try {
+	// 		const data = axios
+	// 			.get('http://kelishamiz.uz/api/v1/user/1', {
+	// 				headers: {
+	// 					Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+	// 				},
+	// 			})
+	// 			.then(res => console.log(res.data))
+	// 			.catch(error => {
+	// 				console.log(error);
 
-					// navigate.push('/login');
-				});
-		} catch (error) {
-			console.log(error);
-		} finally {
-		}
-	}, []);
+	// 				// navigate.push('/login');
+	// 			});
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	} finally {
+	// 	}
+	// }, []);
 
 	return (
 		<>
