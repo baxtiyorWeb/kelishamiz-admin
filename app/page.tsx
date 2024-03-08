0f0c83489f6c5eb03d2bebca1addd83c56c72a6f
baxtiyoeWeb <baxtiyorqurbonnazarov33@gmail.com>
2024-03-08 19:49:07 +0500

    add chart

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,6 @@
 import Home from '@@/components/home/home';
 import UserProvider from '@@/context/UserContext';
 export default function page() {
-	// const { user } = useContext(UserContext);
-	// const navigate = useRouter();
-	// useEffect(() => {
-	// 	try {
-	// 		const data = axios
-	// 			.get('http://kelishamiz.uz/api/v1/user/1', {
-	// 				headers: {
-	// 					Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-	// 				},
-	// 			})
-	// 			.then(res => console.log(res.data))
-	// 			.catch(error => {
-	// 				console.log(error);
-
-	// 				// navigate.push('/login');
-	// 			});
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	} finally {
-	// 	}
-	// }, []);
-
 	return (
 		<>
 			<UserProvider>
