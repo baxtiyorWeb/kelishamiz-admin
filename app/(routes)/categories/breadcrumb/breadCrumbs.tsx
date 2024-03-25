d2336ce92f993dd392e1550cc8c354751b68c036
Baxtiyor Qurbonnazarov <baxtiyorqurbonnazarov33@gmail.com>
2024-03-25 09:31:00 +0500

    ok

--- app/(routes)/categories/breadcrumb/breadCrumbs.tsx
+++ app/(routes)/categories/breadcrumb/breadCrumbs.tsx
@@ -2,10 +2,6 @@
 'use client';
 
 import Link from 'next/link';
-<<<<<<< HEAD
-=======
-import { useParams } from 'next/navigation';
->>>>>>> 30f908962fd5ae2566906ba8bcf74f0d46586d62
 
 interface Category {
 	id: number;
@@ -19,32 +15,12 @@ interface BreadcrumbsProps {
 	data: Category;
 }
 
-<<<<<<< HEAD
 import { useParams } from 'next/navigation';
 const Breadcrumbs = ({ data }: BreadcrumbsProps) => {
 	const { id } = useParams<{ id: string }>();
 
 	function generateBreadcrumbs(node: any, breadcrumbs = []) {
 		if (!node) return breadcrumbs;
-=======
-const Breadcrumb = ({ data }: BreadcrumbProps) => {
-	const params = useParams<{ id: string }>();
-	const breadcrumbs = [];
-
-	let currentData = data;
-	while (currentData) {
-		breadcrumbs.unshift(
-			<span className='text-lg font-bold text-sky-500' key={params.id}>
-				<Link
-					className='hover:underline'
-					href={`/category-details/${currentData.id}`}
-				>
-					{currentData.name}
-				</Link>
-				<span className='text-blue-600'> &gt; </span>
-			</span>
-		);
->>>>>>> 30f908962fd5ae2566906ba8bcf74f0d46586d62
 
 		const updatedBreadcrumbs: any = [...breadcrumbs, node];
 
--- app/(routes)/categories/layout.tsx
+++ app/(routes)/categories/layout.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Breadcrumb from './breadcrumb/breadCrumbs';
 interface BreadcrumbProps {
+	data: any; // Ma'lumotlar obyekti
 	children: React.ReactNode;
 }
 
-const Layout = ({ children }: BreadcrumbProps) => {
+const Layout: React.FC<BreadcrumbProps> = ({ data, children }) => {
 	return (
 		<div>
-<<<<<<< HEAD
 			<Breadcrumb data={data} />
-=======
-			<h1>ok</h1>
-			{/* <Breadcrumb data={data} /> */}
-
->>>>>>> 30f908962fd5ae2566906ba8bcf74f0d46586d62
 			{children}
 		</div>
 	);
